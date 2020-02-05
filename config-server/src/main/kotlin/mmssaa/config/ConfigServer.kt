780edd2df43228aecce6ea02bee2123a7f58ca71
Josh.hong <hong@mz.co.kr>
2020-02-05 18:01:48 +0900

    init

---
+++ config-server/src/main/kotlin/mmssaa/config/ConfigServer.kt
@@ -0,0 +1,13 @@
+package mmssaa.config
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@EnableConfigServer
+@SpringBootApplication
+class ConfigServer
+
+fun main(args: Array<String>) {
+	runApplication<ConfigServer>(*args)
+}
