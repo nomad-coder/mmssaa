c0004c1108edbbb11a82eca029c706ab1249ddfc
Josh.hong <hong@mz.co.kr>
2020-02-07 14:01:27 +0900

    service discovery

---
+++ service-discovery/src/main/kotlin/mmssaa/discovery/ServiceDiscovery.kt
@@ -0,0 +1,13 @@
+package mmssaa.discovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@EnableEurekaServer
+@SpringBootApplication
+class ServiceDiscovery
+
+fun main(args: Array<String>) {
+	runApplication<ServiceDiscovery>(*args)
+}
