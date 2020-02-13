b91362f4b7d36b14beaaef4ab9ecff2ac85b35e8
Josh.hong <hong@mz.co.kr>
2020-02-13 18:27:15 +0900

    .

---
+++ account-service/src/main/kotlin/mmssaa/account/AccountService.kt
@@ -0,0 +1,13 @@
+package mmssaa.account
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@EnableEurekaClient
+@SpringBootApplication
+class AccountService
+
+fun main(args: Array<String>) {
+	runApplication<AccountService>(*args)
+}
