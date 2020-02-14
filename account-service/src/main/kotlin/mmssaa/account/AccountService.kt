82201a7229ed619096d072224610ff07b11724c3
Josh.hong <hong@mz.co.kr>
2020-02-14 10:41:51 +0900

    .

--- account-service/src/main/kotlin/mmssaa/account/AccountService.kt
+++ account-service/src/main/kotlin/mmssaa/account/AccountService.kt
@@ -3,6 +3,7 @@ package mmssaa.account
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,6 +16,7 @@ fun main(args: Array<String>) {
 	runApplication<AccountService>(*args)
 }
 
+@RefreshScope
 @RestController
 class TestController(
 	@Value("\${say.cheese}") private val say: String
