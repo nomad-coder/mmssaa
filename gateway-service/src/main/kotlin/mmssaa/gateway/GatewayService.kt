7151552f7fe4a0979ad162418b8cd17b5fb861b8
Josh.hong <hong@mz.co.kr>
2020-02-18 18:05:27 +0900

    .

---
+++ gateway-service/src/main/kotlin/mmssaa/gateway/GatewayService.kt
@@ -0,0 +1,15 @@
+package mmssaa.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+class GatewayService
+
+fun main(args: Array<String>) {
+	runApplication<GatewayService>(*args)
+}
