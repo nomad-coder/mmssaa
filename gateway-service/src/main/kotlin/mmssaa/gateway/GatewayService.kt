7f75d10cb3b044550ce4db28d2a5430359fbb0a0
Josh.hong <hong@mz.co.kr>
2020-02-18 18:23:51 +0900

    .

--- gateway-service/src/main/kotlin/mmssaa/gateway/GatewayService.kt
+++ gateway-service/src/main/kotlin/mmssaa/gateway/GatewayService.kt
@@ -1,9 +1,14 @@
 package mmssaa.gateway
 
+import com.netflix.zuul.ZuulFilter
+import com.netflix.zuul.context.RequestContext
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @EnableZuulProxy
 @EnableEurekaClient
@@ -12,4 +17,37 @@ class GatewayService
 
 fun main(args: Array<String>) {
 	runApplication<GatewayService>(*args)
+}
+
+@Configuration
+class GatewayConfig {
+
+	@Bean
+	fun preFilter(): ZuulFilter = PreFilter()
+
+}
+
+class PreFilter: ZuulFilter() {
+
+	private val logger = LoggerFactory.getLogger(this.javaClass)
+
+	override fun run(): Any? {
+		val ctx = RequestContext.getCurrentContext()
+		val request = ctx.request
+		logger.info("무엇인가 요청했다. $request")
+		return null
+	}
+
+	override fun shouldFilter(): Boolean {
+		return true
+	}
+
+	override fun filterType(): String {
+//		"pre, routing, post, error"
+		return "pre"
+	}
+
+	override fun filterOrder(): Int {
+		return 0
+	}
 }
